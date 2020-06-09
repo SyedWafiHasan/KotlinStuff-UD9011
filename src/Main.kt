34dc9633127f286b6f55ee99dff8b8e283d96bfd
Syed Wafi Hasan <syedwafihasan@gmail.com>
2020-06-10 00:26:35 +0530

    14 - Add Fish Food - Method 1

--- src/Main.kt
+++ src/Main.kt
@@ -10,7 +10,7 @@ fun main(a: Array<String>)
 fun feedTheFish()
 {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and fish eat $food")
 }
 
@@ -19,4 +19,20 @@ fun randomDay() : String
     val week = listOf ("Sunday", "Monday", "Tuesday",
         "Wednesday", "Thursday", "Friday", "Saturday")
     return week[Random().nextInt(7)]
+}
+
+fun fishFood(day : String) : String
+{
+    var food = "fasting"
+    when (day)
+    {
+        "Sunday" -> food = "Daal Chawal"
+        "Monday" -> food = "Rajma Chawal"
+        "Tuesday" -> food = "Chhola Chawal"
+        "Wednesday" -> food = "Basil Leaves"
+        "Thursday" -> food = "Stuff"
+        "Friday" -> food = "More Food"
+        "Saturday" -> food = "Less Food"
+    }
+    return food
 }
